6287a8655af01a88c568c85d051182db9557121c
Michael Chan <mijoch@gmail.com>
2022-12-03 23:07:01 -0800

    add about page using /app directory page.jsx

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>chantastic</h1>
+      <p>
+        chantastic's is building a creative coding community
+        around curiosity and kindness.
+      </p>
+    </>
+  );
+}
