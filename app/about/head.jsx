f71f217165adfa2958e8145b6a5dc89f55171278
Michael Chan <mijoch@gmail.com>
2022-12-04 02:28:33 -0800

    add head.jsx to /about route to change title and desc

---
+++ app/about/head.jsx
@@ -0,0 +1,12 @@
+export default function AboutHead() {
+  return (
+    <>
+      <title>About chantastic</title>
+      <meta
+        name="description"
+        content="chantastic's is building a creative coding community
+        around curiosity and kindness."
+      />
+    </>
+  );
+}
